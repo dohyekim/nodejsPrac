09fd8ba1bccbb22197c3afe1673c7dbd268be31c
dohyekim <dhleen5@hanmail.net>
2019-09-08 14:54:52 +0900

    Static files

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const express = require('express');
 const app = express(); //express() returns object;
 const port = 3000;
 
+app.use(express.static('public')); //where static files exist
+
+app.get('/imgroute', (req, res) => 
+    res.send('Hello Router, <img src="/star.png">'));
 app.get('/', (req, res) => 
     res.send(`Hello World!`));
-app.get('/login', (req, res) => {
-    res.send('<h1>Login Please</h1>');
-})
+app.get('/login', (req, res) => res.send('<h1>Login Please</h1>'));
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 // const http = require('http');
